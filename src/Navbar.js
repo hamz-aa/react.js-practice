54446d45b378a466dcbb82d37d91327a7c8a6fe5
Hamza <jhamzaahmed@gmail.com>
2024-02-19 16:12:04 +0500

    custom hooks, routes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Scythe Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-
-        {/* adding inline styling in jsx */}
-        <a
-          href="/create"
-          style={{
-            color: "white",
-            backgroundColor: "#f1356d",
-            borderRadius: "8px",
-          }}
-        >
-          New Blog
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
