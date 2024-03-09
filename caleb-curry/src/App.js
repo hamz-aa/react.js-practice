66721766281729aa9a0e4c4ec277b304aa8f7893
Hamza <jhamzaahmed@gmail.com>
2024-03-09 20:57:08 +0500

    added images & classes

--- caleb-curry/src/App.js
+++ caleb-curry/src/App.js
@@ -11,10 +11,16 @@ function App() {
       {showEmployees ? (
         <>
           <input type="text" onChange={(e) => setRole(e.target.value)} />
-          <div className="flex flex-wrap">
-            <Employee name="Hamza" role="Intern" />
-            <Employee name="Osama" role={role} />
-            <Employee name="John" />
+          <div className="flex flex-wrap justify-center">
+            <Employee name="Hamza" role="Intern" img={"assets/person-1.jpg"} />
+            <Employee name="Osama" role={role} img={"assets/person-2.jpg"} />
+            <Employee name="John" img={"assets/person-3.jpg"} />
+            <Employee name="Hamza" role="Intern" img={"assets/person-1.jpg"} />
+            <Employee name="Osama" role={role} img={"assets/person-2.jpg"} />
+            <Employee name="John" img={"assets/person-3.jpg"} />
+            <Employee name="Hamza" role="Intern" img={"assets/person-1.jpg"} />
+            <Employee name="Osama" role={role} img={"assets/person-2.jpg"} />
+            <Employee name="John" img={"assets/person-3.jpg"} />
           </div>
         </>
       ) : (
--- caleb-curry/src/Employee.js
+++ caleb-curry/src/Employee.js
@@ -1,10 +1,10 @@
-export const Employee = ({ name, role }) => {
+export const Employee = ({ name, role, img }) => {
   return (
     <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <img
-        className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
-        src="/img/erin-lindford.jpg"
-        alt="Woman's Face"
+        className="object-cover h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0"
+        src={img}
+        alt="Employee"
       />
       <div className="text-center space-y-2 sm:text-left">
         <div className="space-y-0.5">
