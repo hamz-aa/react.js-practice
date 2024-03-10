6ac7929cf24e0e099433269e18c921558c60b496
Hamza <jhamzaahmed@gmail.com>
2024-03-10 15:52:19 +0500

    mobile styling

--- caleb-curry/src/App.js
+++ caleb-curry/src/App.js
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import "./index.css";
 import Employees from "./pages/Employees";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Header>
-      <Employees />
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/" element={<Employees />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
